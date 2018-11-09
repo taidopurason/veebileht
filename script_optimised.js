48a3c500e5fa417eb38af15113096ce52214c420
Taido Purason <taido.purason@ut.ee>
2018-11-09 12:39:14 +0200

    Optimiseeritud algoritm

--- script_optimised.js
+++ script_optimised.js
@@ -11,6 +11,8 @@ var width = canvas.offsetWidth;
 var height = canvas.offsetHeight;
 var pixels = context.getImageData(0, 0, width, height);
 var started = 0;
+var width_opt = constrain(width, 1, 500)
+var height_opt = constrain(height, 1, 500)
 
 $('#startbtn').click(function() {
 	if (!started){initialise(); draw(); started = 1;}
@@ -36,14 +38,14 @@ function constrain(a,b,c){
 function initialise(){
     canvas.width = document.body.clientWidth;
     canvas.height = document.body.clientHeight;
-    a = createArray(width,height,2);
-    b = createArray(width,height,2);
-    c = createArray(width,height,2);
+    a = createArray(width_opt,height_opt,2);
+    b = createArray(width_opt,height_opt,2);
+    c = createArray(width_opt,height_opt,2);
 	randomise();
 }
 function randomise(){
-	for (x = 0; x < width; x++) {
-        for (y = 0; y < height; y++) {
+	for (x = 0; x < width_opt; x++) {
+        for (y = 0; y < height_opt; y++) {
             a[x][y][p] = Math.random();
             b[x][y][p] = Math.random();
             c[x][y][p] = Math.random();
@@ -54,16 +56,16 @@ function randomise(){
 
 function draw(){
 	var pixelArray = pixels.data;
-    for (x = 0; x < width; x++) {
-		for (y = 0; y < height; y++) {
-		    var c_a = 0.0;
+	for (x=0; x < width_opt; x++){
+		for(y=0; y < height_opt; y++){
+			var c_a = 0.0;
 		    var c_b = 0.0;
 		    var c_c = 0.0;
 		    for (i = x - 1; i <= x+1; i++) {
 			    for (j = y - 1; j <= y+1; j++) {
-			    c_a += a[(i+width)%width][(j+height)%height][p];
-			    c_b += b[(i+width)%width][(j+height)%height][p];
-			    c_c += c[(i+width)%width][(j+height)%height][p];
+			    c_a += a[(i+width_opt)%width_opt][(j+height_opt)%height_opt][p];
+			    c_b += b[(i+width_opt)%width_opt][(j+height_opt)%height_opt][p];
+			    c_c += c[(i+width_opt)%width_opt][(j+height_opt)%height_opt][p];
 			}
 		  }
 		    c_a /= 9.0;
@@ -72,11 +74,15 @@ function draw(){
 		    a[x][y][q] = constrain(c_a + c_a * (k1*c_b - k3*c_c), 0, 1);
 		    b[x][y][q] = constrain(c_b + c_b * (k2*c_c - k1*c_a), 0, 1);
 		    c[x][y][q] = constrain(c_c + c_c * (k3*c_a - k2*c_b), 0, 1);
+		}
+	}
+    for (x = 0; x < width; x++) {
+		for (y = 0; y < height; y++) {
 		  
 		    index = (y * width + x) * 4;
-		    pixelArray[index] = 255 - Math.floor(a[x][y][q] * 100);
-		    pixelArray[index + 1] = 255 - Math.floor(a[x][y][q] * 200);
-		    pixelArray[index + 2] = 255 - Math.floor(a[x][y][q] * 200);
+		    pixelArray[index] = 255 - Math.floor(a[x%width_opt][y%height_opt][q] * 100);
+		    pixelArray[index + 1] = 255 - Math.floor(a[x%width_opt][y%height_opt][q] * 200);
+		    pixelArray[index + 2] = 255 - Math.floor(a[x%width_opt][y%height_opt][q] * 200);
 		    pixelArray[index + 3] = 255;
 		  
 		}
